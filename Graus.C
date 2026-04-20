0abc142fe611416db7cf527bd53e411f26dceacb
AurickJD <126987244+AurocaAmaFarofa@users.noreply.github.com>
2026-04-20 18:54:12 -0300

    Delete Graus.C

--- Graus.C
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void main() {
-    float tempC, tempF;
-    printf("Informe o valor em Fahrenheit: ");
-    scanf("%f", &tempF);
-    tempC = (5* (tempF - 32) /9);
-    printf("Valor em celsius: %f\n", tempC);
-}
